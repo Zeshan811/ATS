cfe787415fa4f3da41961bf4cd02dac054cdcd65
Zeshan811 <zh5849@gmail.com>
2026-05-10 15:39:20 -0600

    adfdk

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
-// || 'https://ats-production-b904.up.railway.app/api';
+//  'https://ats-production-b904.up.railway.app/api';
 
+console.log(import.meta.env);
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: { 'Content-Type': 'application/json' },
