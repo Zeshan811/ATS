c15c466365bcb6dd9e78c8d3cc0b55a4ba75495f
Zeshan811 <zh5849@gmail.com>
2026-05-10 13:14:09 -0600

    ser

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -23,47 +23,14 @@ exports.createJob = async (req, res) => {
 };
 
 // // @route   GET /api/jobs
-// // @desc    Get all jobs (Public
-// exports.getAllJobs = async (req, res) => {
-//   try {
-//     const jobs = await Job.find().sort({ createdAt: -1 }); // Newest first
-//     res.status(200).json(jobs);
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// };
-// @route   GET /api/jobs
-// @desc    Get all jobs (Public with filters)
+// @desc    Get all jobs (Public
 exports.getAllJobs = async (req, res) => {
   try {
-    const { search, branch, department, type } = req.query;
-
-    let filter = {};
-
-    // 🔍 Search by title
-    if (search) {
-      filter.title = { $regex: search, $options: 'i' };
-    }
-
-    if (branch) {
-      filter.branch = branch;
-    }
-
-    if (department) {
-      filter.department = department;
-    }
-
-    if (type) {
-      filter.type = type;
-    }
-
-    const jobs = await Job.find(filter).sort({ createdAt: -1 });
-
-    res.status(200).json({
-      jobs,   // ✅ important (not raw array)
-    });
-
+    const jobs = await Job.find().sort({ createdAt: -1 }); // Newest first
+    res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+// @route   GET /api/jobs
+// @desc    Get all jobs (Public with filters)
